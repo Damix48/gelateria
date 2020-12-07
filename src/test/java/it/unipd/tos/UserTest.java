bb7c86f55bcafa6d0e4dce8027f277a2f6780d77
Damiano Zanardo <damiano.zanardo@gmail.com>
2020-12-07 22:49:35 +0100

    Dimenticato i public

--- src/test/java/it/unipd/tos/UserTest.java
+++ src/test/java/it/unipd/tos/UserTest.java
@@ -21,7 +21,7 @@ public class UserTest {
   int age;
 
   @Before
-  void setup() {
+  public void setup() {
     name = "Damiano";
     age = 21;
 
@@ -29,89 +29,89 @@ public class UserTest {
   }
 
   @Test
-  void testGetName() {
+  public void testGetName() {
     assertEquals(name, user.getName());
   }
 
   @Test
-  void testkGetAge() {
+  public void testkGetAge() {
     assertEquals(age, user.getAge());
   }
 
   @Test(expected = IllegalArgumentException.class)
-  void testNameNull() {
+  public void testNameNull() {
     new User(null, age);
   }
 
   @Test(expected = IllegalArgumentException.class)
-  void testAgeNull() {
+  public void testAgeNull() {
     new User(name, null);
   }
 
   @Test(expected = IllegalArgumentException.class)
-  void testNameAgeNull() {
+  public void testNameAgeNull() {
     new User(null, null);
   }
 
   @Test(expected = IllegalArgumentException.class)
-  void testAgeNegative() {
+  public void testAgeNegative() {
     new User(name, -1);
   }
 
   @Test(expected = IllegalAccessException.class)
-  void testNameEmpty() {
+  public void testNameEmpty() {
     new User("", age);
   }
 
   @Test
-  void testUserUnder18() {
+  public void testUserUnder18() {
     user = new User(name, 17);
     assertTrue(user.isUnder18());
   }
 
   @Test
-  void testUserIs18() {
+  public void testUserIs18() {
     user = new User(name, 18);
     assertFalse(user.isUnder18());
   }
 
   @Test
-  void testUserOver18() {
+  public void testUserOver18() {
     user = new User(name, 19);
     assertFalse(user.isUnder18());
   }
 
   @Test
-  void testEqualsItSelf() {
+  public void testEqualsItSelf() {
     assertTrue(user.equals(user));
   }
 
   @Test
-  void testEqualsUser() {
+  public void testEqualsUser() {
     User user2 = new User(name, age);
     assertTrue(user.equals(user2) && user2.equals(user));
   }
 
   @Test
-  void testDifferentName() {
+  public void testDifferentName() {
     User user2 = new User(name.concat("str"), age);
     assertFalse(user.equals(user2));
   }
 
   @Test
-  void testDifferentAge() {
+  public void testDifferentAge() {
     User user2 = new User(name, age + 1);
     assertFalse(user.equals(user2));
   }
 
   @Test
-  void testDifferentUser() {
+  public void testDifferentUser() {
     User user2 = new User(name.concat("str"), age + 1);
     assertFalse(user.equals(user2));
   }
 
   @Test
-  void testDifferentFromNull() {
+  public void testDifferentFromNull() {
     assertFalse(user.equals(null));
   }
 
