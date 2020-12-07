5a0b245a98d6ce1c07c7451e403228d65b552edf
Damiano Zanardo <damiano.zanardo@gmail.com>
2020-12-07 22:55:09 +0100

    Aggiunto codice User

--- src/main/java/it/unipd/tos/model/User.java
+++ src/main/java/it/unipd/tos/model/User.java
@@ -5,5 +5,33 @@
 package it.unipd.tos.model;
 
 public class User {
+  private final String name;
+  private final int age;
 
+  public User(String _name, int _age) throws IllegalArgumentException {
+    if (_name == null) {
+      throw new IllegalArgumentException("Il nome non può essere nullo");
+    }
+    if (_age < 0) {
+      throw new IllegalArgumentException("L'età non può essere negativa");
+    }
+    if (_name == "") {
+      throw new IllegalArgumentException("Il nome non può essere vuoto");
+    }
+
+    this.name = _name;
+    this.age = _age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public boolean isUnder18() {
+    return age < 18;
+  }
 }
