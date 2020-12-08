bccbe55d9c4274f5ac49fe3b7a1ed7d3afb4bce3
Damiano Zanardo <damiano.zanardo@gmail.com>
2020-12-08 02:13:29 +0100

    Aggiunta eccezione

--- src/main/java/it/unipd/tos/business/exception/TakeAwayBillException.java
+++ src/main/java/it/unipd/tos/business/exception/TakeAwayBillException.java
@@ -6,4 +6,14 @@ package it.unipd.tos.business.exception;
 
 public class TakeAwayBillException extends Exception {
 
+  private static final long serialVersionUID = -6738592842440983810L; // Generato autmaticamente
+  private final String msg;
+
+  public TakeAwayBillException(String error) {
+    msg = error;
+  }
+
+  public String toString() {
+    return "TakeAwayBillException thrown: ".concat(msg);
+  }
 }
