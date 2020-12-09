71e0eeab83cfafe1d0e8f0de109a1eed4cd84358
Damiano Zanardo <damiano.zanardo@gmail.com>
2020-12-09 02:07:28 +0100

    Aggiunto orario al menuitem

--- src/main/java/it/unipd/tos/model/MenuItem.java
+++ src/main/java/it/unipd/tos/model/MenuItem.java
@@ -4,6 +4,8 @@
 
 package it.unipd.tos.model;
 
+import java.time.LocalTime;
+
 public class MenuItem {
   public enum ItemType {
     GELATO, BUDINO, BEVANDA;
@@ -12,6 +14,7 @@ public class MenuItem {
   private final ItemType type;
   private final String name;
   private final double price;
+  private LocalTime time;
 
   public MenuItem(ItemType _type, String _name, double _price) throws IllegalArgumentException {
     if (_type == null) {
@@ -27,9 +30,15 @@ public class MenuItem {
       throw new IllegalArgumentException("Il nome non può essere vuoto");
     }
 
+    time = LocalTime.now();
+
     type = _type;
     name = _name;
     price = _price;
+  }
+
+  public LocalTime getTime() {
+    return time;
   }
 
   public ItemType getType() {
@@ -42,6 +51,17 @@ public class MenuItem {
 
   public double getPrice() {
     return price;
+  }
+
+  public void setTime(LocalTime _time) throws IllegalArgumentException {
+    if (_time == null) {
+      throw new IllegalArgumentException("L'orario non può essere nullo");
+    }
+    if (_time.isAfter(LocalTime.now())) {
+      throw new IllegalArgumentException("L'orario non può essere futuro");
+    }
+
+    time = _time;
   }
 
 }
